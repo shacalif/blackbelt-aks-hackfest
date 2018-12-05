a568c4992f49124fbc2e389ad91e20644e773097
shacalif <32203940+shacalif@users.noreply.github.com>
2018-12-05 17:30:51 -0600

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Just Meee - victo
+        Azure Global Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
