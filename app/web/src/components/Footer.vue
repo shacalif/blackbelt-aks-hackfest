aa712bad82f3844a6bc50f5dc1ee152faf3b9aeb
shacalif <32203940+shacalif@users.noreply.github.com>
2018-12-05 20:55:42 -0600

    Update Footer.vue
    Get

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt - Victorgo
+        I Want My Black Belt
       </div>
       <div class="col-lg-6">
       </div>
